3b9bb1cc19e6c51356796eed40f6737aa26f72c5
Lee Haram <harammon@naver.com>
2021-01-28 21:20:47 +0900

    실습 문제 및 코드

--- Django/실습/1월27일/workapp/urls.py
+++ Django/실습/1월27일/workapp/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 app_name = 'workapp'
 urlpatterns = [
-    path('', views.exercise1, name = 'exercise1'),
+    path('exercise1/', views.exercise1, name = 'exercise1'),
+    path('exercise2/', views.exercise2, name = 'exercise2'),
 ]
--- Django/실습/1월27일/workapp/views.py
+++ Django/실습/1월27일/workapp/views.py
@@ -2,6 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
-def exercise1(request) :
-    template = loader.get_template('exercise1.html')
-    return HttpResponse(template.render(None, request))
+def exercise1(request):
+    template1 = loader.get_template('exercise1.html')
+    return HttpResponse(template1.render(None, request))
+
+def exercise2(request):
+    if request.method == 'POST':        # 템플릿의 form으로부터 호출되는 경우
+        name = request.POST['name']
+        opinion = request.POST['opinion']
+        context = {'name': name, 'opinion': opinion}
+    else:
+        context = None
+    return render(request, 'exercise2.html', context)
